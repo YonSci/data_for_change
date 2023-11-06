e1474f2b783f9bb14349497f05f9e3ae9e287a70
Yonas M <yonas.mersha14@gmail.com>
2023-11-06 07:30:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,32 @@ import pickle
 
 def intro():
        st.sidebar.success("Select a page")
+       st.markdown(
+                """
+                The **Ethiopian Meteorological Institute Climate Data Analysis Tool (EMI-CDAT)** web application is designed to automate the data work flow at the Meteorological Data and Climatology Directorate and to generate charts, graphs, maps, and tabular outputs, as well as intermediate results required for the production of climate bulletins. 
+                
+                **EMI-CDAT** is build & deploy using the [Streamlit](https://docs.streamlit.io/) application development tool. The [Altair](https://altair-viz.github.io/index.html), [Bokeh](https://bokeh.org/), [Holoviews](https://holoviews.org/), [Plotly  Graphing Libraries](https://plotly.com/graphing-libraries/), [SciPy](https://scipy.org/), [NCAR PyNGL](https://www.pyngl.ucar.edu/index.shtml), & [PyNIO](https://www.pyngl.ucar.edu/index.shtml) visualizations packages are used to improve the statistical visualization and mapping capabilities of the App.
+                """
+                )
+       st.markdown(
+                    """
+                    ---
+                    EMI-CDAT App features includes:
+                    - Reading input data in tabular formats such as Microsoft Excel spreadsheet (.xlsx) & comma separate value (.csv) & covert it to a Data-frame.
+                    - The input data includes daily rainfall, maximum & minimum temperature.
+                    - Summary report on the missing data
+                    - Transform the data-frame to netCDF file format.
+                    - Generate basic summary & indices in a tabular formats.
+                    - Produce intermediate results & time series plots.
+                    - Generates maps required for monthly, seasonal (i.e., Bega, Belg, Kiremt), & annual climate bulletins.
+                    """
+                    )
+        
+       st.markdown(
+        """
+        ---
+        """
+        )
        
        
 def Deficiency_Predictor():
