278c440b2dccd1a156d88ad6fcb3b5aa9f9b6496
Yonas M <yonas.mersha14@gmail.com>
2023-11-06 07:25:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import pandas as pd
 import pickle
 
 def intro():
+       st.markdown(f'# {list(page_names_to_funcs.keys())[1]}')
        st.sidebar.success("Select a page")
        
+       
 def Deficiency_Predictor():
+       st.markdown(f'# {list(page_names_to_funcs.keys())[2]}')
        st.title('Vitamin/Mineral Deficiency Predictor')
 
 def Visualization():
+       st.markdown(f'# {list(page_names_to_funcs.keys())[3]}')
        st.title('Vitamin/Mineral Deficiency Predictor')
        
 
